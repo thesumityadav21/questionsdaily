b7d33686166140b1d7d9c857ca0f314718488060
SuMit YaDav <94666370+thesumityadav21@users.noreply.github.com>
2022-09-07 21:46:04 +0530

    Create FEVER.cpp

---
+++ FEVER.cpp
@@ -0,0 +1,31 @@
+https://www.codechef.com/START55D/problems/FEVER
+
+
+MY SOLUTION -
+  
+  #include <iostream>
+using namespace std;
+
+int main() {
+	
+	int n;
+	cin>>n;
+	
+	while(n--){
+	    
+	    int t;
+	    cin>>t;
+	    
+	    if(t>98){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	    
+	}
+	
+	
+	return 0;
+}

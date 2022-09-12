5f4391e825e971ce0b6ff8733b262ed0322329e1
SuMit YaDav <94666370+thesumityadav21@users.noreply.github.com>
2022-09-12 17:08:32 +0530

    Create CWIREFRAME.cpp

---
+++ CWIREFRAME.cpp
@@ -0,0 +1,26 @@
+https://www.codechef.com/submit/CWIREFRAME?tab=statement
+
+MY SOLUTION
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+int t;
+cin>>t;
+
+while(t--){
+    
+    int n,m,x;
+    cin>>n>>m>>x;
+    
+    int s =2*n+2*m;
+    
+    cout<<s*x<<endl;
+    
+}
+	return 0;
+}
